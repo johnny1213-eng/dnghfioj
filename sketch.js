59b381f0a77ea4f10552b2f68805c360e0ba6688
johnny1213-eng <82241742+johnny1213-eng@users.noreply.github.com>
2021-05-20 19:50:34 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var stone,iron,rubber, hammer;
+var sand1,sand2,sand3,sand4,sand5,sand6;
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    stone= new Stone(700,320,100,100);
+   rubber=new Rubber(900,450,70);
+    hammer = new Hammer(10,100);
+
+  sand1=new Sand(505,450,10);
+  sand2=new Sand(510,450,10);
+  sand3=new Sand(515,450,10);
+  sand4=new Sand(520,450,10);
+  sand5=new Sand(525,450,10);
+  sand6=new Sand(530,450,10);
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+stone.display();
+    sand1.display();
+    sand2.display();
+    sand3.display();
+    sand4.display();
+    sand5.display();
+    sand6.display();
+
+    rubber.display();
+}
